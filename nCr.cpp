0df703858e22c85e0d28fc6e056668e14a8707aa
Mashijams <raghavself28@gmail.com>
2023-06-15 00:18:46 +0530

    CP: update nCr file with proper naming conventions

--- nCr.cpp
+++ nCr.cpp
@@ -1,29 +1,33 @@
-ll fact[N], revfact[N];
+#define int long long int
+int	binpow(int a, int b, int m) {int res = 1; while (b > 0) {if (b & 1)res = res * a % m; a = a * a % m; b >>= 1;} return res;}
+
+
+int	fact[N], revfact[N];
 
 void factorial()
 {
-  fact[0] = 1;
+	fact[0] = 1;
 
-  ll i;
+	int i;
 
-  for (i = 1; i < N; i++)
-    fact[i] = i * fact[i - 1] % MOD;
+	for (i = 1; i < N; i++)
+		fact[i] = i * fact[i - 1] % MOD;
 
-  i--;
+	i--;
 
-  revfact[i] = binpow(fact[i], MOD - 2, MOD);
+	revfact[i] = binpow(fact[i], MOD - 2, MOD);
 
-  i--;
+	i--;
 
-  for (; i >= 0; i--)
-    revfact[i] = (i + 1) * revfact[i + 1] % MOD;
+	for (; i >= 0; i--)
+		revfact[i] = (i + 1) * revfact[i + 1] % MOD;
 
 }
 
-ll ncr(ll n, ll r)
+int ncr(int n, int r)
 {
-  if (n < 0 || r < 0 || r > n)
-    return 0;
+	if (n < 0 || r < 0 || r > n)
+		return 0;
 
-  return fact[n] * revfact[r] % MOD * revfact[n - r] % MOD;
+	return fact[n] * revfact[r] % MOD * revfact[n - r] % MOD;
 }
